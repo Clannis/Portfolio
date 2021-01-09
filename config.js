c42ae0297c3c91f277690d407a55e4b3dcd11de4
Clannis <Dodds1990@gmail.com>
2021-01-09 17:07:06 -0600

    first commit

---
+++ config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  siteTitle: 'James Dodds', // <title>
+  manifestName: 'Prologue',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/Logo.png',
+  pathPrefix: `/gatsby-starter-prologue/`, // This path is subpath of your hosting https://domain/portfolio
+  authorName: 'James Dodds',
+  heading: 'Full-Stack Web Developer',
+  // social
+  socialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/Clannis',
+    },
+    {
+      icon: 'fa-twitter',
+      name: 'Twitter',
+      url: 'https://twitter.com/JDodds_Clannis',
+    },
+    {
+      icon: 'fa-facebook',
+      name: 'Facebook',
+      url: 'https://www.facebook.com/james.dodds.5437',
+    },
+    {
+      icon: 'fa-envelope-o',
+      name: 'Email',
+      url: 'mailto:Dodds.James90@gmail.com',
+    },
+  ],
+};
